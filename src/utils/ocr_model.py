cf177cd7874e762b9cdd9144b6b2ef40c39daedb
Khaled Mamdouh <khalidmamdou7@gmail.com>
2024-07-03 07:57:35 +0300

    refactor: add error handling if no ocr models is found

--- src/utils/ocr_model.py
+++ src/utils/ocr_model.py
@@ -3,7 +3,8 @@ import cv2
 import numpy as np
 import easyocr
 import json
-from roboflow import Roboflow
+import requests
+import base64
 
 roboflow_config = json.load(open("./src/utils/roboflow-config.json"))["counters_models"]
 
@@ -31,13 +32,44 @@ def get_digits_from_image(image_path, counter_id = None):
 
 def model_predict_roboflow(image_path, counter_id: str):
     img = cv2.imread(image_path)
+    if counter_id not in roboflow_config:
+        raise Exception("This counter has no defined ocr model yet")
     model_config = roboflow_config[counter_id]
     # url = "https://detect.roboflow.com/"+model_config['project_id']+"/" + model_config['version'] + "?api_key=" + model_config['api_key'] + "&confidence=" + str(model_config['confidence']) + "&overlap=" + str(model_config['overlap'])
-    rf = Roboflow(api_key=model_config["api_key"])
+    # rf = Roboflow(api_key=model_config["api_key"])
 
-    rf_project = rf.workspace().project(model_config["project_id"])
-    rf_model = rf_project.version(model_config["version"]).model
-    rf_result = rf_model.predict(image_path, confidence=model_config["confidence"], overlap=model_config["overlap"]).json()
+    # rf_project = rf.workspace().project(model_config["project_id"])
+    # rf_model = rf_project.version(model_config["version"]).model
+    # rf_result = rf_model.predict(image_path, confidence=model_config["confidence"], overlap=model_config["overlap"]).json()
+    # Construct the URL with query parameters
+    url = "https://detect.roboflow.com/" + model_config['project_id'] + "/" + model_config['version'] + "?"
+    url += "api_key=" + model_config['api_key'] + "&confidence=" + str(model_config['confidence']) + "&overlap=" + str(model_config['overlap'])
+
+    # Open the image in binary mode
+    with open(image_path, 'rb') as image_file:
+        image_data = image_file.read()
+
+    encoded_image = base64.b64encode(image_data)
+    data = {"image": encoded_image}
+
+    # Prepare the request headers (optional, but recommended)
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    # Send POST request with image data
+    response = requests.post(url, data, headers=headers)
+
+    # Check for successful response
+    if response.status_code == 200:
+        # Parse the JSON response
+        rf_result = response.json()
+    else:
+        # Handle error
+        print(f"Error: API request failed with status code {response.status_code}")
+        print(response.text)  # Optional: Print the error message from the response
+        raise Exception("API request failed")
+
     
     predictions = rf_result['predictions']
     image_width = int(rf_result['image']['width'])
